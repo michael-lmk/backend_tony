5e19a0298604d81f178a957506c97f8788abe102
Martinsky Michael <michael@lmksytem.fr>
2024-08-28 17:19:49 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MusicModule } from './music/music.module';
+import { MusicModule } from './module/music/music.module';
+import { MusicModule } from './music/music.module';
+
+@Module({
+  imports: [MusicModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
