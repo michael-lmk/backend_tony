0625e180d4df77fc777b8d9ad44548027a4d90ac
michael <michael@lmksystem.fr>
2024-11-27 12:41:31 +0100

    fix env

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,17 +2,15 @@ import { DataSource } from 'typeorm';
 import 'dotenv/config';
 
 export const dataSourceOptions = new DataSource({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'tony_db',
-    migrationsTableName: 'migrations',
-    entities: [
-        "dist/**/*.entity.js"
-    ],
-    migrations: ["dist/migrations/*.js"],
-    synchronize: true,
-    migrationsRun: true,
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: 3306,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  migrationsTableName: 'migrations',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: true,
+  migrationsRun: true,
 });
